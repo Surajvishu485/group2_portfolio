51e076fe5528fdb3b8f8d368148ce75e646e28f8
Dheeraj <vishwakarmadheeraj2002@gmail.com>
2024-06-04 12:04:55 +0530

    modified

--- src/components/WorkCData.js
+++ src/components/WorkCData.js
@@ -13,35 +13,35 @@ const ProjectCardData = [
        title:"Vande_Bharat News.",
        text:"This application is build to render news as per user's choice. It renders news including highlights and user's can visit to get full news by hitting the card components.For selection of choices few links are there in navbar. This vande-bharat-news-app is designed using NEWS-API.",
        view:"https://github.com/Surajvishu485/vande-bharat-news-app",
-       source:"https://github.com/Priyankavishu49/vande-bharat-news-app"
+       source:"https://github.com/Priyankavishu49/vande_bharat_newsApp"
     },
     {
         imgsrc: pro2,
         title:"Quiz Application.",
         text:"Quiz applications are designed to conduct a test in hiring like aptitude round in hiring a condidate. This is simple application used to render few questions under timer. You have to choose the right alternative and result will be displayed when you complete your test within time.",
         view:"https://github.com/Surajvishu485/quiz-website",
-        source:"https://github.com/Priyankavishu49/Quiz-website"
+        source:"https://github.com/Priyankavishu49/Quiz_App"
      },
      {
         imgsrc: pro3,
         title:"Drum-kit application",
         text:"Music is very crucial in our day to day life to deal with tough situation. This website used to produce piano's different sound to heal you..",
         view:"https://github.com/Surajvishu485/Drum_kit",
-        source:"https://github.com/Priyankavishu49/drum-kit"
+        source:"https://github.com/Priyankavishu49/Drum_Kit"
      },
      {
         imgsrc: pro4,
         title:"Calculator...",
         text:"Performing calculation is very important in our day to day life. This application is designed using HTML,CSS and JavaScript. This is used to perform mathematical operation like addition, substraction, multiplication and division etc..",
         view:"https://github.com/Surajvishu485/Calculator",
-        source:"https://github.com/Priyankavishu49/calculator"
+        source:"https://github.com/Priyankavishu49/Calculator"
      },
      {
         imgsrc: pro5,
         title:"ToDo Application..",
         text:"As we know in toda's world there is so much pressure. This application helps you to remind and store task list as per your priority. It's helps you to choose an important task early.",
         view:"https://github.com/Surajvishu485/ToDo-Application",
-        source:"https://github.com/Priyankavishu49/To-do-application"
+        source:"https://github.com/Priyankavishu49/To_Do-Application"
      }
    //   {
    //      imgsrc: pro3,
